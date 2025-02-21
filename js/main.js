c9c9bbf3f66bf764ebe23397d6553aa65817818a
excellent-softDev <excellentmokoena60@gmail.com>
2025-02-21 18:17:23 +0200

    site completed

--- js/main.js
+++ js/main.js
@@ -281,18 +281,18 @@ function aosInit() {
   /**
    * Correct scrolling position upon page load for URLs containing hash links.
    */
-//   window.addEventListener('load', function(e) {
-//     if (window.location.hash) {
-//       if (document.querySelector(window.location.hash)) {
-//         setTimeout(() => {
-//           let section = document.querySelector(window.location.hash);
-//           let scrollMarginTop = getComputedStyle(section).scrollMarginTop;
-//           window.scrollTo({
-//             top: section.offsetTop - parseInt(scrollMarginTop),
-//             behavior: 'smooth'
-//           });
-//         }, 100);
-//       }
-//     }
-//   });
+  window.addEventListener('load', function(e) {
+    if (window.location.hash) {
+      if (document.querySelector(window.location.hash)) {
+        setTimeout(() => {
+          let section = document.querySelector(window.location.hash);
+          let scrollMarginTop = getComputedStyle(section).scrollMarginTop;
+          window.scrollTo({
+            top: section.offsetTop - parseInt(scrollMarginTop),
+            behavior: 'smooth'
+          });
+        }, 100);
+      }
+    }
+  });
 
